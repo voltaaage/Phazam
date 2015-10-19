356d3ae7f16061aed6e419eba5bd33e989500a17
Fong <fong.fan.90@gmail.com>
2015-10-18 23:47:05 -0500

    remove images.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'images/index'
-
-  get 'images/show'
-
   devise_for :users
     resources :users, only: [:update, :create, :index]
 
--- images.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'flickraw'
-
-
-FlickRaw.api_key = "d296c5f2ccc1a545222bcdff8cde7d12"
-FlickRaw.shared_secret = "311e521aa3f3afe1"
-
-
