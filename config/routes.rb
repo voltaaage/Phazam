4295c9df9c864d3648e91fe9a8a6434f7b9195ac
Fong <fong.fan.90@gmail.com>
2015-12-25 02:44:07 -0600

    bootstrap customizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   devise_for :users
-  resources :users, only: [:show, :index]
+  resources :users, only: [:index,:show]
 
   resources :images, only: [:index, :show, :update]
   resources :challenges
