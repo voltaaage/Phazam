e0736e9e60f62304569b1744b4fca8abe9779de4
Fong <fong.fan.90@gmail.com>
2016-01-10 14:44:23 -0800

    refactor of params hash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     root_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,6 +10,16 @@ class ImagesController < ApplicationController
   private
 
   def image_params
-    params.require(:image).permit(:photo_id,:title,:square_url,:medium_url,:original_url,:focal_length,:exposure,:aperture,:iso_speed)
+    params.require(:image).permit(
+      :photo_id,
+      :title,
+      :square_url,
+      :medium_url,
+      :original_url,
+      :focal_length,
+      :exposure,
+      :aperture,
+      :iso_speed
+    )
   end
 end
